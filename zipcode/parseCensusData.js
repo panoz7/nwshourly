775593005fba6b34685df6450eed717b57723350
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-02-03 17:34:20 -0500

    parse census zip data to json

---
+++ zipcode/parseCensusData.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const file = process.argv[2]
+
+const data = fs.readFileSync(file, 'utf8');
+
+let entries = data.split("\n");
+entries.shift();
+
+let zipData = [];
+entries.forEach(entry => {
+    let values = entry.split("\t");
+    let zip = values[0];
+    if (zip) {
+        let lat = values[5];
+        let long = values[6].trim();
+        zipData.push([zip, lat, long])
+    }
+})
+
+
+fs.writeFileSync('zipdata.json', JSON.stringify(zipData), 'utf8');
+
