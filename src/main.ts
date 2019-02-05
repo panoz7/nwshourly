bd1f5a4dafd736aa357a8bf72a48e0d8f57f3b16
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-02-04 21:40:16 -0500

    Add ability to switch zip codes

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,41 @@ import { getNwsHourly, NwsProperty } from './nwsHourly.js';
 import { roundDate } from './helper.js';
 import { NwsDisplay, TempDisplay, PercentDisplay, NumericDisplay } from './nwsDisplay.js';
 
-const displays = []
+let displays = []
+let startTime: Date;
+let endTime: Date;
 
 setupDisplays(20009);
 
 document.getElementById('hours').addEventListener('change', (e) => {
-    // console.log(e.target.value);
     let element = e.target as HTMLInputElement;
     let hours: number = parseInt(element.value);
 
-    let startTime = roundDate(new Date, 'h');
-    let endTime = new Date(startTime.getTime() + ((hours - 1) * 60 * 60 * 1000))
+    startTime = roundDate(new Date, 'h');
+    endTime = new Date(startTime.getTime() + ((hours - 1) * 60 * 60 * 1000))
     
     displays.forEach(display => display.renderDisplay(startTime, endTime))
+})
+
+document.getElementById('zipCode').addEventListener('change', (e) => {
+    let element = e.target as HTMLInputElement;
+    let zipCode: number = parseInt(element.value);
+
+    setupDisplays(zipCode);
+    
 })
 
 async function setupDisplays(zipCode: number): Promise<void> {
     const data = await getNwsHourly(zipCode);
 
+    displays = [];
     displays.push(new TempDisplay(document.getElementById('tempData'), data.hourlyTemp, 45, 35));
-    displays.push(new PercentDisplay(document.getElementById('skyCoverData'), data.skyCover, [255,255,255]));
+    displays.push(new PercentDisplay(document.getElementById('skyCoverData'), data.skyCover, [190,190,190]));
     displays.push(new PercentDisplay(document.getElementById('precipProbabilityData'), data.precipProbability, [0,0,255]));
-    displays.push(new NumericDisplay(document.getElementById('windSpeedData'), data.windSpeed, [255,255,255], 20));
+    displays.push(new NumericDisplay(document.getElementById('windSpeedData'), data.windSpeed, [190,190,190], 20));
 
-    let startTime: Date = roundDate(new Date, 'h');
-    let endTime: Date = new Date(startTime.getTime() + (23 * 60 * 60 * 1000))
+    startTime = roundDate(new Date, 'h');
+    endTime = new Date(startTime.getTime() + (23 * 60 * 60 * 1000))
 
     displays.forEach(display => display.renderDisplay(startTime, endTime))
 
